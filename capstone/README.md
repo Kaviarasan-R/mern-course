8da7d0d55bb3e7c9b7a832438fb3abcaf703b06f
Kaviarasan-R <kaviarasanofficial26@gmail.com>
2025-11-15 16:36:10 +0530

    Added capstone frontend project

--- capstone/README.md
+++ capstone/README.md
@@ -1,1 +1,3 @@
-# Capstone - Final Project
+# Capstone
+
+The Books Blog System is a full-stack application built with React 19, TypeScript, Tailwind v4, shadcn/ui, and Redux, offering a responsive interface with pagination, filtering, sorting, and secure user interactions. The backend uses Express.js, MongoDB, and JWT with access-refresh token flow to provide scalable data management and reliable authentication.
--- capstone/frontend/src/main.tsx
+++ capstone/frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
-import { fetchBooksData } from "./slice/BooksSlice";
+import { store } from "./store/store.ts";
+import { fetchBooksData } from "./slice/BooksSlice.tsx";
 import { fetchUsers } from "./slice/UsersSlice.tsx";
 import { BrowserRouter } from "react-router-dom";
 
