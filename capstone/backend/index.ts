af46213836262216941516b00ea9849df0ac71d8
Kaviarasan-R <kaviarasanofficial26@gmail.com>
2025-11-22 22:19:41 +0530

    Renamed server.ts to index.ts

--- capstone/backend/index.ts
+++ capstone/backend/index.ts
@@ -50,6 +50,6 @@ app.use((err: any, req: any, res: any, next: any) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("App is listening on port 3000...");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("App is listening...");
 });
